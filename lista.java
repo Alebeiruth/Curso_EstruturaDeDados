c68ddd927c932385d286ed5dbe662f88946e9ec2
Alexandre Beiruth <101944430+Alebeiruth@users.noreply.github.com>
2023-12-12 16:51:10 -0300

    Update lista.java

--- lista.java
+++ lista.java
@@ -64,6 +64,94 @@ public class ListaEstatica<T> implements Lista<T> {
     dados[pos] = valor; //insere o dado
     tamanho = tamanho + 1; //aumenta o tamanho
   }
+  //chamada do metodo
+  public void adicionar(T valor) {
+    adicionar(tamanho, valor);
+  }
+  
+  //metodo remover 
+  public T remover(int pos) {
+    Objects.checkIndex(pos, tamanho);
+
+  T dado = dados[pos]; //dado a ser retornado
+  //move os elementos para a esquerda
+    for(int i = pos; i < tamanho -1; i++) {
+      dados[i] = dados[i+1];
+  }
+    dados[tamanho-1] = null; //elimina o ultimo dado
+    tamanho = tamanho - 1; //reduz o tamanho da lista
+    return dado.;
+  }
+  
+  //metodo limpar
+  public void limpar(){ //remover a referencia de todos os elementos e zera a variavel tamanho
+    for (int i = 0; i < getTamanho(); i++) {
+      dados[i] = null;
+    }
+    tamanho = 0;
+  }
+  
+  //metod get > obter um dados sem remove-lo
+  public T get(int pos) {
+    Objects.checkIndex(0, tamanho);
+    return dados[pos];
+  }
+  
+  //metodo set > substituie um dado em uma posicao especifica
+  public void set(int pos, T valor) {
+    Objects.checkIndex(pos, tamanho);
+    dados[pos] = valor;
+  }
+  //metodo de busca
+  public int indice(T valor) {
+    for (int i = 0;  i < tamanho; i++) {
+      if (Objects.equals(valor, dados[i])) {
+        return i;
+      }
+    }
+    return -1;
+  }
+  //metodo de busca
+  public int ultimoIndice(T valor) {
+    for (int i = tamanho -1; i >= 0; i--) {
+      if(Objects.equals(valor, dados[i])) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
+ 
+  //metodo iteração > percorre a lista, elemento a elemento
+  int atual = -1;
+
+  //ha um proximo elemento?
+  while ((atual+1) < getTamanho()) {
+  //move para o proximo elemento e o retorna
+  atual = atual + 1;
+  T dado = dados[atual];
+
+  //usa o elemento
+  System.out.println(dado);
+}
+
+ public interface Lista<T> extends Iterable <T> {
+    private class ListaIterator implements Iterator <T> {
+       int atual = -1;
+
+      public boolean hasNext() {
+        return (atual+1) < getTamanho();
+      }
+      
+  public T next() {
+    atual = atual + 1;
+    return dados[atual];
+  }
+} 
+
+  
+
+  
   
   
   
